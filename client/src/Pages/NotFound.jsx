e2dec015639e980168163fa9c84cdb6ac51c91b6
Michael Owusu Darko <darkolyrical@gmail.com>
2025-05-06 15:18:39 +0000

    fixed 404 error

--- client/src/Pages/NotFound.jsx
+++ client/src/Pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div  className='min-h-screen my-auto flex flex-col items-center justify-center'>
+    <div  className='min-h-screen my-auto flex flex-col bg-amber-50 items-center justify-center'>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
       <p className='text-amber-600'>
