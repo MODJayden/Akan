c97d7b681e0beba79c6054eb4bf386d3664a6cac
Michael Owusu Darko <darkolyrical@gmail.com>
2025-05-22 01:28:31 +0000

    jk

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(passport.session());
 app.use(express.json());
 app.use(
   cors({
-    origin: " https://akan-gken.onrender.com/",
+    origin: " https://akan-gken.onrender.com",
     credentials: true,
     methods: "GET,PUT,POST,DELETE",
   })
