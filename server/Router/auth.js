dd86fab5940b4c0b8877278b7d4c8b8549b8ed38
Michael Owusu Darko <darkolyrical@gmail.com>
2025-05-22 20:13:07 +0000

    a

--- server/Router/auth.js
+++ server/Router/auth.js
@@ -22,7 +22,7 @@ router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
-    res.redirect("http://akan-gken.onrender.com/");
+    res.redirect("http://akan-gken.onrender.com");
   }
 );
 
